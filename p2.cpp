08e8ed3d53b6cb12e068e2f470f2585e6df2c8d3
fanfan <57862422+Vegetable-Code@users.noreply.github.com>
2021-01-14 01:51:25 +0800

    p2.cpp
    进行了函数代码优化，添加注释

--- p2.cpp
+++ p2.cpp
@@ -2,6 +2,7 @@
 #include <conio.h>
 #include <stdlib.h>
 #define MAX_NUM 100
+
 // 输入函数 
 int intput(int arr[], int len) {
 	for(int i = 0; i < len; i++) {
@@ -22,6 +23,7 @@ int *sort1(int a[], int len, char style) {
 	int i, j, temp;
 	// 升序 
 	if(style == 'a') {
+		// 冒泡排序 
 		for(j = 0; j < len - 1; j++) {
 			for(i = 0; i < len - 1; i++) {
 				if(p[i] < p[i + 1]) {
@@ -32,6 +34,7 @@ int *sort1(int a[], int len, char style) {
 	}
 	// 降序 
 	if(style == 'd') {
+		// 冒泡排序
 		for(j = 0; j < len - 1; j++) {
 			for(i = 0; i < len - 1; i++) {
 				if(p[i] > p[i + 1]) {
@@ -43,10 +46,10 @@ int *sort1(int a[], int len, char style) {
 	return p; 
 } 
 
-// 通用输出函数 
+// 输出函数 
 int *output(int a[], int len, char style, int *(*fun)(int a[], int n, char style)) {
-	int *p = NULL;
-	p = (*fun)(a, len, style);
+	int *p = NULL; // 指针定义 
+	p = (*fun)(a, len, style); // 调用指针 
 	printf("\n%d名同学的成绩为：", len);
 	for(int i = 0; i < len; i++) {
 		printf("%d ", *(p + i));
@@ -56,9 +59,10 @@ int *output(int a[], int len, char style, int *(*fun)(int a[], int n, char style
 }
 
 int main() {
+	// 变量定义 
 	char style;
-	int n, a[10], b[MAX_NUM];
-	
+	int n, a[10], b[MAX_NUM];	
+	// 功能实现 
 	printf("请输入10名同学的成绩：");
 	intput(a, 10);
 	int len = sizeof(a) / sizeof(a[0]); // 获取长度 
@@ -66,7 +70,6 @@ int main() {
 	printf("\n请选择你想要的排序方式："); 
 	scanf("%c", &style);
 	output(a, len, style, sort1);
-	
 	printf("\n请输入总人数（人数应当小于%d）：", MAX_NUM);
 	scanf("%d", &n);
 	printf("\n请输入%d名同学的成绩：", n);
@@ -77,3 +80,11 @@ int main() {
 	scanf("%c", &style);
 	output(b, len, style, sort1);
 } 
+  
+/* 示例 
+9 8 7 6 1 2 3 4 5 0
+a
+10
+9 8 7 6 5 1 2 3 4 0 
+d
+*/ 
